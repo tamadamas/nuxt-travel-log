06d8397ca25f4b62dc57edb4f3b95f97e21f4da3
Tomas Tamadamas <1058433+tamadamas@users.noreply.github.com>
2025-06-11 20:19:53 +0200

    Add eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
   },
+
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
+  modules: ["@nuxt/eslint"],
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
 });
